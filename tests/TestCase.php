31531aab51edbf3636a8de7c496be7bdb7aa962b
Andrés Bedoya <andres@bedoya.co>
2025-06-22 11:44:03 -0500

    Added Test environment to work with SQLite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,30 @@
 
     abstract class TestCase extends BaseTestCase
     {
+        /**
+         * Configure the tests to use SQLite in memory
+         *
+         * @param $app
+         *
+         * @return void
+         */
+        protected function getEnvironmentSetUp($app): void
+        {
+            $app['config']->set('database.default', 'sqlite');
+            $app['config']->set('database.connections.sqlite', [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+            ]);
+        }
+
+        /**
+         * Package provider
+         *
+         * @param $app
+         *
+         * @return string[]
+         */
         protected function getPackageProviders ( $app ): array
         {
             return [
